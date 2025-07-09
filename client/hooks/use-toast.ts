7aad6b4d7897d914cc5c255b99eb246d04ea1001
arlagaddajagruthi <arlagaddajagruthi@gmail.com>
2025-07-09 16:35:23 +0530

    REACT-LANDING-JAGRUTHI

--- client/hooks/use-toast.ts
+++ client/hooks/use-toast.ts
@@ -87,8 +87,7 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action;
 
-      // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
+      
       if (toastId) {
         addToRemoveQueue(toastId);
       } else {
@@ -185,4 +184,5 @@ function useToast() {
   };
 }
 
-export { useToast, toast };
+export { toast, useToast };
+
